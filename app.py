f80a4d579f599840e18207a1742f23403726da7a
MINJU207 <jmj040308@gmail.com>
2024-11-18 12:00:20 +0900

    네이버 api 활용 마무리

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template
+import nvapi
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,14 +15,23 @@ def baseball():
 def naver():
     return render_template('naver.html')
 
-@app.route('/youtube')
-def youtube():
-    return render_template('youtube.html', name=keyword)
-
 @app.route('/ytpage')
 def ytpage():
     return render_template('ytpage.html')
 
+@app.route('/youtube', methods=['GET', 'POST'])
+def youtube():
+    linklist = ['https://www.youtube.com/embed/wsk95Mq0Cys?si=wdVWrmJJY7hnX-c0', 'https://www.youtube.com/embed/z6TgbMB4mms?si=E6VMYPWjARq_a-mj']
+    keyword = request.form["keyword"]
+    print(keyword)
+    linknum = 0 # 0은 이영지,  1은 임영웅
+    if keyword == '이영지':
+        linknum = 0
+    elif keyword == '임영웅':
+        linknum = 1
+    else:
+        print('잘못된입력')
+    return render_template('youtube.html', name=keyword, link=linklist[linknum])
 
 @app.route('/method', methods=['GET', 'POST'])
 def method():
@@ -29,7 +40,9 @@ def method():
     else:
         keyword = request.form["keyword"]
         print(keyword)
-        return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
+        data = nvapi.blog(keyword)
+        # return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
+        return render_template("result.html", keyword=keyword, blist=data)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
